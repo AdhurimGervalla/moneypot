dd09d0b8a46d98340bed0a9dc9181d2f812aca40
Adhurim Gervalla <adhurim@gervalla.ch>
2022-10-21 00:08:22 +0200

    add new field to pot model

--- models/Pot.ts
+++ models/Pot.ts
@@ -2,6 +2,7 @@ export default interface Pot {
     id: string,
     uid: string,
     name: string,
+    creation_date: number,
     resetDate?: number,
     takeOverValue?: number,
     description?: string
